ce58eb9582aaafb9ef78f5713217e75489a71fb1
MyBoyDoru <denis.leonte123@gmail.com>
2020-08-11 13:53:33 +0200

    I hope this will do

---
+++ html-files/javaScripts/index.js
@@ -0,0 +1,12 @@
+$(window).on("load",function(){
+    $(window).scroll(function() {
+        var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+        $(".fade").each(function() {
+            var objectBottom = $(this).offset().top + $(this).outerHeight();
+
+            if(objectBottom < windowBottom) {
+                if($(this).css("opacity") == 0) { $(this).fadeTo(500, 1);}
+            }
+        });
+    }).scroll();
+});
